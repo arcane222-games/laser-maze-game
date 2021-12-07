e9968badd7327166401f58502e1a52b16ecf0ee3
Onggae22 <hong3883@naver.com>
2021-12-08 00:00:44 +0900

    fix. Separate LazerMazeGame v1 & v2  - separate version 1 & version 2

--- src/com/arcane22/lazermazegame/Main.java
+++ src/com/arcane22/lazermazegame/Main.java
@@ -1,8 +1,35 @@
 package com.arcane22.lazermazegame;
 
+import com.arcane22.lazermazegame.v1.GameThread;
+
+
+/**
+ * @class Main
+ * @author Lee Hong Jun (arcane22, hong3883@naver.com)
+ * @description
+ *  - Last modified 2021. 12. 07
+ */
+
 public class Main {
-    public static void main(String[] args) {
-        GameThread thread = new GameThread();
-        thread.start();
+    public static void main(String[] args) throws Exception {
+
+        // Get game version from program argument
+        String version = args[0];
+
+        // Separate game version
+        switch(version) {
+            case "v1":
+                GameThread thread = new GameThread();
+                thread.start();
+                System.out.println("Run game version 1");
+                break;
+
+            case "v2":
+                System.out.println("Run game version 2");
+                break;
+
+            default:
+                throw new Exception("UndefinedVersionTypeException");
+        }
     }
 }
